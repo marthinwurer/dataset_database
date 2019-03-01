3241581741f06bfbd7f06f735143731bc3c07c2b
Benjamin Maitland <marthinwurer@gmail.com>
2019-02-28 23:43:45 -0500

    Added more flags and tqdm stuff

--- database_builder.py
+++ database_builder.py
@@ -3,7 +3,10 @@ import glob
 import os
 import tempfile
 
+from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import sessionmaker
+from tqdm import tqdm
 
 from SQLTypes import Item
 
@@ -15,12 +18,12 @@ python database_builder.py --wipe /mnt/nas/datasets/database.db /mnt/nas/dataset
 
 def store_image_paths(base_dir, session):
     types = [".jpg", ".png", ".gif"]
-    for root, dirs, files in os.walk(base_dir):
+    for root, dirs, files in tqdm(os.walk(base_dir)):
         print(files)
         basenames = [os.path.splitext(file) for file in files]
         print(basenames)
         image_files = filter(lambda x: os.path.splitext(x)[1] in types, files)
-        for file in image_files:
+        for file in tqdm(image_files):
             full_path = os.path.join(root, file)
             try:
                 item = Item(path=full_path)
@@ -30,7 +33,7 @@ def store_image_paths(base_dir, session):
 
 
 def store_item_urls(base_dir, session):
-    for root, dirs, files in os.walk(base_dir):
+    for root, dirs, files in tqdm(os.walk(base_dir)):
         print(root, dirs, files)
         images_dir = os.path.join(root, "IMAGES")
         # find images files
@@ -38,7 +41,7 @@ def store_item_urls(base_dir, session):
             print("found url file: %s" % (file,))
 
             with open(file) as f:
-                for line in f:
+                for line in tqdm(f):
                     line = line.strip()
                     try:
                         item = Item(url=line)
@@ -52,6 +55,10 @@ def main():
     parser = argparse.ArgumentParser(description='Short sample app')
 
     parser.add_argument('--wipe', action="store_true")
+    parser.add_argument('--urls', action="store_true")
+    parser.add_argument('--images', action="store_true")
+    parser.add_argument('--hash', action="store_true")
+
     parser.add_argument('database')
     parser.add_argument('base_dir')
 
@@ -61,18 +68,20 @@ def main():
 
     database_path = args.database
     base_dir = args.base_dir
-    conn = sqlite3.connect(database_path)
-    # create a table
-    if args.wipe:
 
-        conn.close()  # close then reopen after it's wiped
+    if args.wipe:
         print("Wiping database at %s" % (database_path,))
         os.remove(database_path)
-        conn = sqlite3.connect(database_path)
-        Item.build_table(conn)
 
-    # store_image_paths(args.base_dir, conn)
-    store_item_urls(base_dir, conn)
+    engine = create_engine('sqlite:///%s' % (database_path,))
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    if args.images:
+        store_image_paths(args.base_dir, session)
+
+    if args.urls:
+        store_item_urls(base_dir, session)
 
     # download missing urls
     temp_dir = tempfile.mkdtemp()
